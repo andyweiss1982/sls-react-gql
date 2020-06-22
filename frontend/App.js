e3f06f2ce2560bb899ac13a73bebf1eafb23a038
Andy Weiss <andyweiss1982@gmail.com>
2020-06-22 13:24:22 -0400

    cognito

--- frontend/App.js
+++ frontend/App.js
@@ -6,14 +6,31 @@ import { AuthProvider } from "./Authentication";
 import Authorization from "./Authorization";
 import Tasks from "./Tasks";
 
+import Amplify, { Auth } from "aws-amplify";
+// import { withAuthenticator, AmplifySignOut } from "@aws-amplify/ui-react";
+
+Amplify.configure({
+  Auth: {
+    region: process.env.REGION,
+    userPoolId: process.env.USER_POOL,
+    userPoolWebClientId: process.env.USER_POOL_CLIENT,
+  },
+});
+
+console.log({ Amplify, Auth });
+
 const App = () => (
   <ApolloProvider client={client}>
     <AuthProvider>
       <Authorization>
+        {/* <AmplifySignOut /> */}
         <Tasks />
       </Authorization>
     </AuthProvider>
   </ApolloProvider>
 );
 
-ReactDOM.render(<App />, document.getElementById("root"));
+// const AuthedApp = withAuthenticator(App);
+const AuthedApp = App;
+
+ReactDOM.render(<AuthedApp />, document.getElementById("root"));
